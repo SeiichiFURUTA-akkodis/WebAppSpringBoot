a259172d3d1f83e3eafe7d9d1f5408e02f34f4b1
SeiichiFURUTA <SeiichiFURUTA@TRN-TTC-L02-03>
2024-06-13 10:03:01 +0900

    初期設定

---
+++ src/test/java/jp/co/akkodis/webapp/WebAppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.akkodis.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
